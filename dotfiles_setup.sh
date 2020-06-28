e237dc667331ac2d0ec8b2f746fdeab2e9c24e1f
Brian Kustra <briank@pecsafety.com>
2020-06-27 22:32:33 -0500

    use different linking flags

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -4,6 +4,6 @@ dotfiles=(".vimrc" ".profile" ".tmux.conf")
 dir="${HOME}/dotfiles"
 
 for dotfile in "${dotfiles[@]}";do
-    ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}"
-    echo "${dir}/${dotfile}" "${HOME}/${dotfile}"
+    ln -sfv "${dir}/${dotfile}" "${HOME}/${dotfile}"
+    # echo "${dir}/${dotfile}" "${HOME}/${dotfile}"
 done
